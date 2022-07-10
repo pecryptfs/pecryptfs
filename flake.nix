fe9e5fa35ec7ba352fcdb39c6b9e10d0fbdbb684
Ingo Ruhnke <grumbel@gmail.com>
2022-07-10 14:15:00 +0200

    Add flake8 and unitest to flake.nix checkPhase

--- flake.nix
+++ flake.nix
@@ -2,28 +2,44 @@
   description = "Somewhat Python incomplete reimplementation of eCryptfs";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    nix.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nix, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        pythonPackages = pkgs.python310Packages;
       in rec {
-        packages = flake-utils.lib.flattenTree {
-          pecryptfs = pkgs.python3Packages.buildPythonPackage rec {
+        packages = flake-utils.lib.flattenTree rec {
+          pecryptfs = pythonPackages.buildPythonPackage rec {
             pname = "pecryptfs";
             version = "0.1.0";
             src = nixpkgs.lib.cleanSource ./.;
-            nativeBuildInputs = with pkgs.python3.pkgs; [
-            ];
-            propagatedBuildInputs = with pkgs.python3.pkgs; [
+            propagatedBuildInputs = with pythonPackages; [
               pycrypto
             ];
-           };
+            checkInputs = (with pkgs; [
+              pyright
+            ]) ++ (with pythonPackages; [
+              flake8
+              mypy
+              pylint
+              types-setuptools
+            ]);
+            checkPhase = ''
+              runHook preCheck
+              flake8 pecryptfs tests
+              # pyright pecryptfs tests
+              # mypy pecryptfs tests
+              # pylint pecryptfs tests
+              python3 -m unittest discover -v -s tests/
+              runHook postCheck
+            '';
+          };
+          default = pecryptfs;
         };
-        defaultPackage = packages.pecryptfs;
-      });
+      }
+    );
 }
